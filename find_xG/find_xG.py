2473577f0b1f0c13abfc88f4c3b5620de40ef800
Alex Brown <alexbrown47@virginmedia.com>
2026-03-16 23:36:25 +0000

    A few small changes

--- find_xG/find_xG.py
+++ find_xG/find_xG.py
@@ -6,7 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
-
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from plotting_functions.plot_pitch import plot_half_pitch
 
 shots_df = pd.read_parquet('data/shots.parquet')
 
@@ -26,10 +29,10 @@ plt.show()
 plt.scatter(shots_df['angle'], encoded_result)
 plt.show()
 
-X = shots_df[['distance_from_goal_center','angle']]
+my_X = shots_df[['distance_from_goal_center','angle']]
 
 # get train and test data # 
-x_train, x_test, y_train, y_test = train_test_split(X, encoded_result, train_size=0.8, random_state=47)
+x_train, x_test, y_train, y_test = train_test_split(my_X, encoded_result, train_size=0.8, random_state=47)
 
 model = lm.LogisticRegression(class_weight='balanced')
 model.fit(x_train, y_train)
@@ -49,7 +52,41 @@ print(auc)
 
 
 # Visualise the xG # 
-x = np.linspace(60,120,200)   # only attacking half
-y = np.linspace(0,80,200)
+x = np.linspace(0,1,100)   # only attacking half
+y = np.linspace(0,1,100)
 
 xx, yy = np.meshgrid(x,y)
+
+goal_x = 1
+goal_y = 0.5
+
+distance = np.sqrt((goal_x - xx)**2 + (goal_y - yy)**2)
+angle = np.arctan2(abs(goal_y - yy), goal_x - xx)
+
+grid_features = np.column_stack([distance.ravel(), angle.ravel()])
+
+grid_features = pd.DataFrame(
+    grid_features,
+    columns=['distance_from_goal_center', 'angle']
+)
+xg_surface = model.predict_proba(grid_features)[:,1]
+
+xg_surface = xg_surface.reshape(xx.shape)
+
+plt.figure(figsize=(8,6))
+
+plt.imshow(
+    xg_surface,
+    extent=(0,50,0,70),
+    origin='lower',
+    cmap='viridis'
+)
+
+plot_half_pitch()
+
+plt.colorbar(label="xG Probability")
+
+plt.title("Expected Goals Surface")
+plt.savefig('find_xG/xG_surface.png', dpi=150, bbox_inches='tight')
+
+plt.show()
